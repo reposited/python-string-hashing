4ddc4aa6709654667e13adf2264ceadfee7b78d3
Reposited <kazi@riseup.net>
2023-08-19 23:28:41 -0700

    Added user ability to input strings for hashing

--- app.py
+++ app.py
@@ -16,3 +16,38 @@ print(f"\nSHA256 hash of '{x}':")
 print(hash_x.hexdigest())
 
 # List of hashes: https://docs.python.org/3/library/hashlib.html
+
+# ----------------------------------------------------------------
+print("<------------------------>")
+print("")
+print("< User Input For String Hashing >")
+print("")
+# ----------------------------------------------------------------
+
+# Adding user input for own user hashing.
+userinput = str(input("Display a message you would like to hash: "))
+
+# Print user message.
+print(f"\nThe input message is: '{userinput}'") 
+# Note: \n is a type of escape character that will create a new line when used.
+
+# Produce a SHA256 hash from a string.
+msg = userinput
+
+# Encode the string as bytes.
+encode_msg = msg.encode()
+
+# Hash the string.
+hash_msg = hashlib.sha256(encode_msg)
+
+# Results.
+print(f"\nSHA256 hash of '{msg}':") 
+
+# Convert it to a hex string.
+print(hash_msg.hexdigest())
+
+# ----------------------------------------------------------------
+
+# To-do: 
+# - Convert msg hash to string.
+# - Add filtering for adding more hashes.
